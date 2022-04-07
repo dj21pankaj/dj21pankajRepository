724e5e433a89eb3253bd1903f581aa00a35f3f19
dj21pankaj.Repository <dj21.pankaj@gmail.com>
2022-04-07 14:32:45 +0530

    Commit message

---
+++ src/Java_class/Intruduction.java
@@ -0,0 +1,13 @@
+package Java_class;
+
+public class Intruduction {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I AM PANKAJ");
+
+
+		
+	}
+	
+}
---
+++ src/Java_class/Method1s.java
@@ -0,0 +1,13 @@
+package Java_class;
+
+public class Method1s {
+	public static void test3() {
+	
+	System.out.println("Running class Method1s in test3");
+
+	
+
+	}
+	
+
+}
